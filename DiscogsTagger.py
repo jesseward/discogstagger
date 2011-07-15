c6d1a85c1eab52c67aec8113ec331d55650261c1
Jesse Ward <jesse.ward@gmail.com>
2011-07-15 12:37:45 -0400

    Completed rough version of the Album class

--- DiscogsTagger.py
+++ DiscogsTagger.py
@@ -2,6 +2,9 @@ import discogs_client as discogs
 import re
 
 class Album(object):
+    """ Wraps the discogs-client-api script, abstracting the minimal set of
+        artist data required to tag an album/release """
+
 
     def __init__ (self, releaseid):
 
@@ -11,62 +14,88 @@ class Album(object):
         except:
             print "error"
 
-        self.alb_images = self.__album_images()
-        self.title = self.__album_title()
-        self.year = self.__album_year()
-        self.tracks = self.__album_tracks()
-        self.genre = self.__album_genre()
-        self.artist = self.__album_artist()
-        #self.alb_label
+    def __str__(self):
+        
+        return "<%s - %s>" % (self.artist, self.title)
 
     def album_info(self):
         """ ...  """
+        
         div = "_ _______________________________________________ _ _\n"
         r = div
         r += " Name : %s - %s\n" % (self.artist, self.title)
-        #r += "Label : %s\n" % (self.label)
+        r += "Label : %s\n" % (self.label)
         r += "Genre : %s\n" % (self.genre)
-        #r += "Catno : %s\n" % (self.cat_num)
+        r += "Catno : %s\n" % (self.catno)
         r += " Year : %s\n" % (self.year)
-        r += "  URL : http:/www.discogs.com/release/%s\n" % (self.release)
+        r += "  URL : http:/www.discogs.com/release/%s\n" % (self.release._id)
         r += div
         for key, value in self.tracks.items():
-            r += "%.2d. %s - %s\n" % (key, self.artist, self.tracks[key])
-
+            r += "%.2d. %s - %s\n" % (key, value[0], value[1])
         return r
 
-    def __album_images(self):
-        """ return a single list of images for the given album """
-        return [ x['uri'] for x in self.release.data['images'] ]
+    @property
+    def catno(self):
+        """ Returns the release catalog number """
 
-    def __album_title(self):
-        """ return the album release name from discogs API """
-        return self.release.title
+        return self.release.data['labels'][0]['catno']
+
+    @property
+    def label(self):
+        """ Returns the release Label name """
+
+        return self.release.data['labels'][0]['name']
     
-    def __album_year(self):
+    @property
+    def images(self):
+        """ return a single list of images for the given album """
+
+        return [ x['uri'] for x in self.release.data['images'] ]
+    
+    @property
+    def title(self):
+        """ return the album release name from discogs API """
+
+        return self.release.title
+   
+    @property 
+    def year(self):
         """ returns the album release year obtained from API 2.0 """
+
         good_year = re.compile('\d\d\d\d')
         try:
             return good_year.match(str(self.release.data['year'])).group(0)
         except IndexError:
             return '1900'
-
-    def __album_genre(self):
+    
+    @property
+    def genre(self):
         """ obtain the album genre """ 
+
         return self.release.data['styles'][0]
 
-    def __album_artist(self):
+    @property
+    def artist(self):
         """ obtain the album artist """
+
         artist = self.release.artists[0]
-        return  artist.data['name']
-
-    def __album_tracks(self):
+        return  self.__clean_name(artist.data['name'])
+    
+    @property
+    def tracks(self):
         """ provides the tracklist of the given release id """
-        tlist = [ tracklist['title'] for tracklist in self.release.tracklist \
-                 if tracklist['type'] == 'Track' ]
-        return dict(zip(xrange(1, len(tlist)+1), tlist))
+        
+        tracklist = {}
+        for i, t in enumerate(self.release.tracklist):
+            if t['type'] == 'Track':
+                try:
+                    artist = t['artists'][0].name
+                except IndexError:
+                    artist = self.artist
+                tracklist[i+1] = [artist, t['title']]
+        return tracklist
 
-    def __clean_name(self, clean_target, reposition=True):
+    def __clean_name(self, clean_target):
         """ Cleans up the format of the artist or label name. 
             Examples:
                 Goldie (12) , becomes Goldie
@@ -80,11 +109,10 @@ class Album(object):
         # remove discogs duplicate handling eg : John (1)
         clean_target = re.sub('\s\(\d+\)', '', clean_target)
 
-        if reposition:
-            for regex in groups:
-                if re.search(r'%s' % regex, clean_target):
-                    clean_target = "%s %s" % (groups[regex], re.search('%s' % regex,\
-                    clean_target).group(1))
+        for regex in groups:
+            if re.search(r'%s' % regex, clean_target):
+                clean_target = "%s %s" % (groups[regex], re.search('%s' % regex,\
+                clean_target).group(1))
         return clean_target
 
 class FileAction(object):
